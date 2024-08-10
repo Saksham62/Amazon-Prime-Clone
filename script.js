176d44415ab225a8c3b329a36e9513c8be8a89f5
Saksham Mangla <sakshammangla279@gmail.com>
2024-08-10 11:50:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+document.querySelector(".first").getElementsByClassName(".svg")
+document.querySelector(".extras").getElementsByClassName(".svg")
+
+svg1.addEventListener("mouseenter", () => {
+    svg1.src="svg/arrow down white.svg";
+}
+)
+svg1.addEventListener("mouseleave", () => {
+    svg1.src="svg/arrow down.svg";
+}
+)
+
+svg2.addEventListener("mouseenter", () => {
+    svg2.src="svg/arrow down white.svg";
+}
+)
+svg2.addEventListener("mouseleave", () => {
+    svg2.src="svg/arrow down.svg";
+}
+)
+
+svg3.addEventListener("mouseenter", () => {
+    svg3.src="svg/arrow down white.svg";
+}
+)
+svg3.addEventListener("mouseleave", () => {
+    svg3.src="svg/arrow down.svg";
+}
+)
+
+svg4.addEventListener("mouseenter", () => {
+    svg4.src="svg/arrow down white.svg";
+}
+)
+svg4.addEventListener("mouseleave", () => {
+    svg4.src="svg/arrow down.svg";
+}
+)
